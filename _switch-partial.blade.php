498cafd00a9590edfed637cf3d1e5ed81c5ae07e
Chris Tudhope <44896935+cartoonclouds@users.noreply.github.com>
2019-02-22 10:31:55 +1030

    Update _switch-partial.blade.php

--- _switch-partial.blade.php
+++ _switch-partial.blade.php
@@ -7,11 +7,11 @@ function flatten($arr) {
 }
 @endphp
 <div class="form-group switch-component">
-    {{ Form::label(str_slug($label,'_'), $label, ['class' => 'control-label']) }}
+    <label class="control-label" for="{{ $name }}">{!! $label !!}</label>
     <label class="switch switch-text switch-{{ $color }}">
         <input type="checkbox"
-               name="{{ str_slug($label,'_') }}"
-               {{ old(str_slug($label,'_'), $value) ? 'checked="checked"' : '' }}
+               name="{{ $name }}"
+               {{ old($name, $value) ? 'checked="checked"' : '' }}
                value="1"
                {!! flatten(array_merge_recursive(['class' => 'switch-input'], array_diff_key($attributes, array_flip(['type', 'name', 'value'])))) !!}>
         <span class="switch-label" data-on="{{ $text[0] }}" data-off="{{ $text[1] }}"></span>
